73074606904e68f4bb956f353933928c020260fe
csh0711 <christian.schwoerer@novatec-gmbh.de>
2021-02-10 19:15:36 +0100

    Add KDoc for JsonMatcher

--- src/test/kotlin/info/novatec/JsonMatcher.kt
+++ src/test/kotlin/info/novatec/JsonMatcher.kt
@@ -5,6 +5,11 @@ import org.hamcrest.TypeSafeMatcher
 import org.skyscreamer.jsonassert.JSONCompare
 import org.skyscreamer.jsonassert.JSONCompareMode.NON_EXTENSIBLE
 
+/**
+ * Hamcrest matcher that checks JSON.
+ *
+ * @see <a href="https://github.com/nt-ca-aqe/micronaut-library-app/blob/master/library-service/src/test/kotlin/utils/JsonMatcher.kt">AQE's Library App</a>
+ */
 class JsonMatcher(private val expectedJson: String) : TypeSafeMatcher<String>() {
 
     companion object {
